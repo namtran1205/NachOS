b4530c0361a1bf6c69c6e8607f5e2cc5f4e88465
tamthwong <86826192+tamthwong@users.noreply.github.com>
2024-04-01 20:35:22 +0700

    Update ascii

--- nachos/nachos-3.4/code/test/ascii.c
+++ nachos/nachos-3.4/code/test/ascii.c
@@ -1,43 +1,6 @@
 #include "syscall.h"
 #include "copyright.h"
 
-int int2char(int x, char str[])
-{
-    int startPoint = 0;
-    if (x < 0)
-    {
-        startPoint++;
-        x *= -1;
-    }
-
-    int pw = 1, cnt = 1;
-
-    // tinh so chu so cua so
-    while (x / pw > 9) 
-    {
-        pw *= 10;
-        cnt++;
-    }
-
-    for(int i = 0; i < cnt; i++)
-    {
-        str[startPoint + i] = x / pw + 48;
-        x %= pw;
-        pw /= 10;
-    }
-    if (startPoint) str[0] = '-';
-    str[cnt + startPoint] = '\0';
-    return cnt + startPoint;
-}
-
-void filePrintInt(int x, OpenFileID fileID)
-{
-    char *num = (char*) maloc(sizeof(char) * 20);
-    int len = int2char(x, num);
-    Write(num, len, fileID);
-    free(num);
-}
-
 int main()
 {
     // mo file mota bang system call Open
@@ -65,11 +28,11 @@ int main()
     // => can ghi vao file cac ki tu
     if (isCreate || fileID < 0) return 0;
 
-    Write("Ascii  table:\n", 15, fileID);
+    Write("Ascii  table:\n", 14, fileID);
 
     for(i = 32; i < 127; i++)
     {
-        filePrintInt(i, fileID);
+        WriteInt(i, fileID);
         Write(asciiCharacter, 7, fileID);
         asciiCharacter[4]++; // cap nhat thanh ki tu tiep theo
     }
